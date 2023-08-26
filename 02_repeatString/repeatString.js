84de20c642e8c5639f80d2a87e8bf659d772cf92
Jhann Quiambao <jimbao.dev@gmail.com>
2023-08-26 00:06:31 -0400

    Finish exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function (word, times) {
+  let i = 0;
+  let message = "";
 
+  if (times < 0) {
+    message = "ERROR";
+  } else {
+    for (i = 0; i < times; i++) {
+      message += word;
+    }
+  }
+
+  return message;
 };
 
 // Do not edit below this line
