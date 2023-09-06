4a86874935e762a67f95db65a689eb4f7fe06932
Jhann Quiambao <jimbao.dev@gmail.com>
2023-09-06 17:30:01 -0400

    finish exercise 8-9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,31 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
+    // lower case the entire sentence
+    sentence = sentence.toLowerCase();
 
+    // removes spaces from the sentence
+        sentence = sentence.replaceAll(" ", "");
+
+    // removes ! from the sentence
+        sentence = sentence.replace("!", "");
+
+    // removes . from the sentence
+        sentence = sentence.replace(".", "");
+        
+    // removes , from the sentence
+    do {
+        sentence = sentence.replace(",", "");
+    } while (sentence.includes(","));
+
+    let reverse = "";
+    for (let i = sentence.length; i >=0; i--){
+        reverse += sentence.charAt(i);
+    }
+    console.log("sentence: " + sentence);
+    console.log("reverse: " + reverse);
+    if (sentence === reverse) return true;
+    else return false;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(length) {
+let curr = 0 ;
+let next = 1;
+for (let i = 0; i< length; i++){
+next = curr + next;
+next ++;
+}
+return next;
 };
 
 // Do not edit below this line
