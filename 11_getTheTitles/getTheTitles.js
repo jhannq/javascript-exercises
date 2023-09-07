d69526544821229ff7fb2919ae308b62ccdb1161
Jhann Quiambao <jimbao.dev@gmail.com>
2023-09-07 10:28:13 -0400

    Finish exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(book) {
+let arr = [];
+for (let i =0; i< book.length; i++){
+    arr.push(book[i].title);
+}
+return arr;
 };
 
 // Do not edit below this line
