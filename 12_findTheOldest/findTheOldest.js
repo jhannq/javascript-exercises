f19441eee911cb60544e99f33f3e266a75f028a2
Jhann Quiambao <jimbao.dev@gmail.com>
2023-09-07 11:31:11 -0400

    wip exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let oldest = 0;
+    let oldestIndex = 0;
+    for (let i = 0; i<arr.length; i++){
+        // check if person has yearOfDeath
+        // if not add current year 
+        if (!('yearOfDeath' in arr[i])){
+            arr[i].yearOfDeath = 2023;
+        }
+        // get years lived
+        // if years lived bigger than max
+        // max = years lived
+        if ((arr[i].yearOfDeath - arr[i].yearOfBirth) > oldest){
+            oldest = arr[i].yearOfDeath - arr[i].yearOfBirth;
+            oldestIndex = i;
+        }
+    }
+    return arr[oldestIndex].name;
 };
 
 // Do not edit below this line
